a363f46b754491c136daf9b39690bab30a6d38f2
mohd arif <alliswell5solution@gmail.com>
2025-05-18 21:23:31 +0530

    latest check

--- src/api/weatherAPI.js
+++ src/api/weatherAPI.js
@@ -4,6 +4,7 @@ const API_KEY = import.meta.env.VITE_API_KEY;
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 console.log("API_KEY:", import.meta.env.VITE_API_KEY);
 console.log("BASE_URL:", import.meta.env.VITE_BASE_URL);
+console.log("Hello")
 
 
 export const fetchWeatherByCity = async (city) => {
